df6873c28957db54d6842241428915a66f15a197
George Nyangaya <george.nyangaya@gmail.com>
2025-09-04 13:01:01 +0300

    Added db connectivity

---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine, text
+
+engine = create_engine('postgresql+psycopg2://postgres.obycmztoqwzxzfoguklc:*t9Z4fYL3yhGzs.@aws-1-eu-north-1.pooler.supabase.com:5432/postgres')
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        results = conn.execute(text("""
+        SELECT
+        *
+        FROM
+        jobs;
+        """))
+
+    
+        jobs = []
+        for row in results.all():
+            jobs.append(row._mapping)
+
+    return jobs
