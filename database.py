6d9b163365cd294b63407c46d09b5d62768a0092
George Nyangaya <george.nyangaya@gmail.com>
2025-09-04 14:01:03 +0300

    Fixed db connectivity again

--- database.py
+++ database.py
@@ -2,13 +2,13 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
 
-engine = create_engine('postgresql+psycopg2://postgres.obycmztoqwzxzfoguklc:*t9Z4fYL3yhGzs.@aws-1-eu-north-1.pooler.supabase.com:5432/postgres')
-
 # Load environment variables from the .env file
 load_dotenv()
 
 # Access the connection string from the environment variable
 db_connection_string = os.environ.get("DB_CONNECTION_STRING")
+
+engine = create_engine(db_connection_string)
 
 def load_jobs_from_db():
     with engine.connect() as conn:
